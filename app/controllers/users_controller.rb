41e9370e17ef6154da1d3a22ba424a106d5faa98
新井　雄介 <ay088055@gmail.com>
2019-08-27 19:40:07 +0900

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,25 +17,18 @@ class UsersController < ApplicationController
 
 	def edit
 		@user =User.find(params[:id])
-		if @user.id !=current_user.id
-			redirect_to root_path
-		end
 	end
 
 	def update
 		@user = User.find(params[:id])
-		if@user.update(use_params)
-			redirect_to user_path(current_user)
-		else
-			render :edit
-		end
+		@user.update(user_params)
+		redirect_to user_path(current_user.id)
 	end
 
 	def destroy
 		user =User.find(params[:id])
-		if user.destroy
-			redirect_to root_path
-		end
+		user.destroy
+		redirect_to root_path
 	end
 
 private
